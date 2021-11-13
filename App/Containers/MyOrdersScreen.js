f7096779604b70c252ab19d0345f35857f95e52b
MuhammadUsman <m.usmanqau786@gmail.com>
2021-11-13 05:03:20 +0500

    dummy data added

--- App/Containers/MyOrdersScreen.js
+++ App/Containers/MyOrdersScreen.js
@@ -2,27 +2,8 @@ import React from 'react'
 import { OrderItem } from "../Components";
 import { FlatList, StyleSheet, View } from 'react-native'
 import { MetricsHorizontal } from "../Theme/Metrics";
+import DATA_LIST from '../DataAssets/OrderHistory.json'
 
-const DATA_LIST = [
-  {
-    'orderID': '#2BTVG',
-    'date': '2021-11-08T18:44:33.928Z',
-    'time': '2021-11-08T18:44:33.928Z',
-    'price': '$14.46',
-    'mealName': 'Avocado Toast',
-    'restaurantName': 'NU Kitchen Somerville',
-    'orderStatus': 'Processing',
-  },
-  {
-    'orderID': '#2BTVH',
-    'date': '2021-11-08T18:44:33.928Z',
-    'time': '2021-11-08T18:44:33.928Z',
-    'price': '$15.46',
-    'mealName': 'Avocado Toast',
-    'restaurantName': 'NU Kitchen Somerville',
-    'orderStatus': 'Completed',
-  }
-]
 
 const MyOrdersScreen = (props) => {
   const renderItem = ({ item }) => {
